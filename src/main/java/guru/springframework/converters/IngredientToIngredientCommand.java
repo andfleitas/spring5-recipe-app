12f6a18b1d06cd0e871ebd29e529904cd7aeb507
andres.fleitas <andres.fleitas@smartmatic.com>
2020-09-12 18:19:43 -0500

    viewing ingredient details

--- src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
+++ src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
@@ -41,6 +41,9 @@ public class IngredientToIngredientCommand implements Converter<Ingredient, Ingr
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setAmount(ingredient.getAmount());
         ingredientCommand.setUnitOfMeasure(uomConverter.convert(ingredient.getUnitOfMeasure()));
+        if (ingredient.getRecipe() != null) {
+            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
+        }
         return ingredientCommand;
     }
 }
---
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -0,0 +1,21 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.IngredientCommand;
+
+/**
+ * The interface Ingredient service.
+ * @author andres
+ */
+public interface IngredientService {
+
+    /**
+     * Find by recipe id and ingredient id ingredient command.
+     *
+     * @param recipeId
+     *         the recipe id
+     * @param ingredientId
+     *         the ingredient id
+     * @return the ingredient command
+     */
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
